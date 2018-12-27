8c7b12a40cf289044e7364c7f442c5983d36944d
Bertrand Juglas <bertrand.juglas@ozitem.com>
2018-12-27 23:40:30 +0100

    output ip and renaming of droplet

--- instance.tf
+++ instance.tf
@@ -1,8 +1,11 @@
 # Create a new Web Droplet in the nyc2 region
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "pchain" {
   image  = "ubuntu-16-04-x64"
   name   = "bert-pchain"
   region = "blr1"
   size   = "s-4vcpu-8gb"
   ssh_keys = ["${digitalocean_ssh_key.pchain.fingerprint}"]
+}
+output "ip" {
+  value = "${digitalocean_droplet.pchain.ipv4_address}"
 }
